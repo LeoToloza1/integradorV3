06273f354f5c85ba45c34b5bba122472f6d1d820
Toloza  Leonel <leotoloza6@gmail.com>
2023-09-19 17:14:03 -0300

    segundo commit

--- app/app.js
+++ app/app.js
@@ -1,2 +1,11 @@
 import  Express from "express";
+import router from "./rutas/rutas.js";
 const app = Express();
+const port = process.env.PORT || 8080;
+app.use('/',router);
+
+
+
+app.listen(port,()=>{
+    console.log(`Corriendo en el puerto: ${port}`);
+});
