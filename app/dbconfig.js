feef49a793e6a2052491d3db9443b0a065c13369
Toloza  Leonel <leotoloza6@gmail.com>
2023-09-26 20:13:22 -0300

    deploy-AhoraSIIII

--- app/dbconfig.js
+++ app/dbconfig.js
@@ -1,10 +1,15 @@
 import mysql from "promise-mysql";
 const config={
     host: 'mysql-leotoloza.alwaysdata.net',
-    port: 3306,
     user: 'leotoloza',
     password: 'LeonelSantiagoToloza113346',
     database: 'leotoloza_web'
 };
-
+try {
+    const conn = await mysql.createConnection(config);
+    console.log('Conexión a la base de datos exitosa');
+    await conn.end();
+} catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+}
 export const conn = await mysql.createConnection(config);
